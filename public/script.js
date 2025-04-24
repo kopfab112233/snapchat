d9e79defbdbed8d90a7118e08c1b617f85ee6d89
kopfab112233 <lucaalexanderu@gmail.com>
2025-04-24 18:06:43 +0200

    Add files via upload

---
+++ public/script.js
@@ -0,0 +1,9 @@
+function goToStep2() {
+  document.getElementById("step1").style.display = "none";
+  document.getElementById("step2").style.display = "block";
+}
+
+function goToStep1() {
+  document.getElementById("step2").style.display = "none";
+  document.getElementById("step1").style.display = "block";
+}
