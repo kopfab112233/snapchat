f2cd5df6bbc7f560ad9cd59fba2dff528e6db9e2
kopfab112233 <lucaalexanderu@gmail.com>
2025-04-25 19:15:47 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const colors = require("colors");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
