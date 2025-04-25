8aeb09dc6a5a393924c201c17e64606e41348494
kopfab112233 <lucaalexanderu@gmail.com>
2025-04-25 19:31:43 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
